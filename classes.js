3cf74f1e65f11d5faa25559c745e407ff29fc6f9
James <42189301+James-Is-A-Name@users.noreply.github.com>
2018-09-05 18:33:49 +1200

    added second div colour

--- classes.js
+++ classes.js
@@ -5,20 +5,26 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one();
+  two();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('blue');
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two');
+
+  two.classList.toggle('green');
+}
 
 // CREATE FUNCTION three HERE
 
